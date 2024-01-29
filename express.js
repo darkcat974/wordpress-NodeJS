ba03dca88a0200fd1871a702b5099ee8562bbc06
darkcat974 <florian.dajon@epitech.eu>
2024-01-29 23:29:19 +0400

    yes

--- express.js
+++ express.js
@@ -1,33 +1,56 @@
-const fs = require('fs');
-const express = require('node:express');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = express.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  console.log('serveur créer');
-  const path = req.url.toString()
+const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const port = 3001
 
-  if (path == '/accueil') {
-      console.log("Vous êtes a l’accueil");
-      res.writeHead(200, {"Content-Type": "text/html"});
-      res.write('<p>Bienvenue sur l’accueil</p>')
-      res.end();
-  }
-  if (path == '/profil') {
-      console.log("Vous êtes sur votre profil");
-      res.writeHead(200, {"Content-Type": "text/html"});
-      res.write('<p>Bienvenue sur votre profile</p>')
-      res.end();
-  }
-  if (path == '/Logo') {
-      console.log("Vous êtes au logo");
-      res.writeHead(200, {"Content-Type": "text/html"});
-      res.write('<img src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Epitech.png">')
-      res.end();
-  }
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://noah:julWAvKT742ZCPXR@mydb.exhfxs3.mongodb.net/');
 
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const { Schema } = mongoose;
+
+const MyModel = mongoose.model('workshop', new Schema({id: {type: String, required: true}, student: {type: String, required: true}}))
+
+mongoose.Promise = global.Promise
+
+
+app.use(bodyParser.json());
+
+app.get('/accueil', (req, res) => {
+    console.log("Vous êtes a l’accueil");
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write('<p>Bienvenue sur l’accueil</p>')
+    res.end();
+}
+)
+
+app.get('/profil', (req, res) => {
+    console.log("Vous êtes sur votre profil");
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write('<p>Bienvenue sur votre profile</p>')
+    res.end();
+}
+)
+
+app.get('/Logo', (req, res) => {
+    console.log("Vous êtes au logo");
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write('<img src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Epitech.png">')
+    res.end();
+}
+)
+
+app.get('/db', (req, res) => {
+    console.log("mongotruc");
+    MyModel.find().then((humeurs) => {return res.status(200).json({humeurs})});
+}
+)
+
+app.get('/db2', (req, res) => {
+    console.log("mongotruc");
+    MyModel.findOne().then((humeurs) => {return res.status(200).json({humeurs})});
+}
+)
+
+app.listen(port, () => {
+  console.log('serveur crée');
+})
